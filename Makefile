47f99af323dd14a26ef97a1ea44b27d5aaad6d4c
Ivan Chechkin <i.g.chechkin@yandex.ru>
2022-05-30 13:17:46 +0300

    pleh

--- Makefile
+++ Makefile
@@ -1,13 +1,16 @@
 CSOURCES := $(shell find . -name "*.c")
 CPPSOURCES := $(shell find . -name "*.cpp")
-CFLAGS := -Wall -Wextra -Wfloat-equal -O  -MMD -Wstrict-prototypes
+CFLAGS := -Wall -Wextra -Wfloat-equal -O  -MMD
 
 debug: CFLAGS := $(CFLAGS) -g
 all: CFLAGS := $(CFLAGS) -g -fsanitize=address -fsanitize=leak -fsanitize=undefined -fsanitize=null -fsanitize=bounds-strict -fstack-protector-all
 coverage: CFLAGS := $(CFLAGS) -g -fsanitize=address -fsanitize=leak -fsanitize=undefined -fsanitize=null -fsanitize=bounds-strict -fstack-protector-all -fprofile-arcs -ftest-coverage
 werror: CFLAGS := $(CFLAGS) -Werror -g -fsanitize=address -fsanitize=leak -fsanitize=undefined -fsanitize=null -fsanitize=bounds-strict -fstack-protector-all
 
-CXXFLAGS := $(CFLAGS)
+debug: CXXFLAGS := $(CFLAGS) -std=c++17
+all: CXXFLAGS := $(CFLAGS) -std=c++17 -lssl -lcrypto
+coverage: CXXFLAGS := $(CFLAGS) -std=c++17
+werror: CXXFLAGS := $(CFLAGS) -std=c++17
 
 LDLIBS := -lm -lpthread -lstdc++
 all: LDFLAGS := -fsanitize=address -fsanitize=leak -fsanitize=undefined -fsanitize=null -fsanitize=bounds-strict -fstack-protector-all
@@ -28,3 +31,4 @@ clean:
 	rm -f main
 	rm -f *.o
 	rm -f *.d
+	rm -f *.gc*
---
+++ main.cpp
@@ -0,0 +1,61 @@
+#define CPPHTTPLIB_OPENSSL_SUPPORT
+#include "httplib.h"
+#include <iostream>
+#include <string>
+#include <map>
+
+
+int main(int argc, char** argv){
+  httplib::Client ipCli("https://whatsmyip.dev");
+
+  //httplib::Request req("/ip");
+
+  auto res = ipCli.Get("/api/ip");
+  std::string ip;
+  std::istringstream f(res->body);
+  std::getline(f, ip, ',');
+  ip.erase(0, 9);
+  ip.erase(--ip.end());
+  ip = '/' + ip;
+  httplib::Client geoCli("https://json.geoiplookup.io");
+
+  res = geoCli.Get(ip.c_str());
+
+  //std::cout << res->body << "\n";
+
+  std::istringstream g(res->body);
+  std::string line;
+  std::string latitude;
+  std::string longitude;
+  while(std::getline(g, line)){
+    std::istringstream l(line);
+    std::string result;
+
+    if(std::getline(l, result, ':')){
+      //std::cout << result << std::endl;
+      if(result == "    \"latitude\""){
+        std::getline(l, latitude, ' ');
+        std::getline(l, latitude);
+      }
+      if(result == "    \"longitude\""){
+        std::getline(l, longitude, ' ');
+        std::getline(l, longitude);
+      }
+    }
+  }
+
+  latitude.erase(--(--(--latitude.end())), latitude.end());
+  longitude.erase(--(--(--longitude.end())), longitude.end());
+
+  std::cout << latitude << ' ' << longitude << std::endl;
+
+  httplib::Client weatherCli("https://api.openweathermap.org");
+
+  std::string task = "/data/2.5/onecall?lat=" + latitude + "&lon=" + longitude + "&units=metrix&exclude=current&appid=3c5d356723d2e5638a9938834df8734c";
+  std::cout << task << std::endl;
+  res = weatherCli.Get((task).c_str());
+  std::cout << "here\n";
+  std::cout << res->body << std::endl;
+
+  return 0;
+}
